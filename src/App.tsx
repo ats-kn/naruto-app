ff79bfe65bcd0bd140c0bcbaf1e336c09b7b6bcc
ats <atsushi.k214@gmail.com>
2024-07-04 23:29:32 +0900

    スクリーンにGETしてきたキャラクターの画像を表示（画像が抜けている場合は，ダミーを表示）

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
-import { useEffect } from 'react';
+// React とそのフック、スタイルシート、axios をインポート
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
+// App コンポーネントの定義
 function App() {
+  // キャラクター情報を保持するための状態
+  const [characters, setCharacters] = useState([]);
+
+  // コンポーネントのマウント時にキャラクター情報を取得
   useEffect(() => {
     fetchCharacters();
   }, []);
 
+  // API からキャラクター情報を取得する非同期関数
   const fetchCharacters = async () => {
     const apiUrl = 'https://narutodb.xyz/api/character';
     const response = await axios.get(apiUrl);
+    setCharacters(response.data.characters);
   };
+
+  // UI のレンダリング
   return (
-    <div className="App">
-      hello world
+    <div className="container">
+      <main>
+        <div className="cards-container">
+          {/* 取得したキャラクター情報をマッピングしてカードを表示 */}
+          {characters.map((character: { id: number, images: string[] }) => {
+            return (
+              <div className="card" key={character.id}>
+                <img 
+                  src={
+                    // キャラクターの画像があればそれを、なければダミー画像を表示
+                    character.images[0] != null
+                      ? character.images[0]
+                      : 'dummy.png'
+                  }
+                  alt="character" 
+                  className="card-image"
+                />
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </div>
   );
 }
 
+// App コンポーネントをエクスポート
 export default App;
