dd8fddeb2818f798ac9b8467d9de28e7a315033f
ats <atsushi.k214@gmail.com>
2024-07-04 22:24:24 +0900

    narutoDBのcharacterのGET処理

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = async () => {
+    const apiUrl = 'https://narutodb.xyz/api/character';
+    const response = await axios.get(apiUrl);
+  };
   return (
     <div className="App">
       hello world
