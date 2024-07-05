f8d65dc990033475224ea30342fcff0593468041
ats <atsushi.k214@gmail.com>
2024-07-05 23:36:35 +0900

    前ページに戻る処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import { Character } from './type';
 
 // App コンポーネントの定義
 function App() {
-  // キャラクター情報を保持するための状態変数と、ページ番号を保持するための状態変数を定義
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const [page, setPage] = useState(1);
+  const [characters, setCharacters] = useState<Character[]>([]); // キャラクター情報を保持するための状態変数
+  const [page, setPage] = useState(1); // ページ番号を保持するための状態変数
+  const [isLoading, setIsLoading] = useState(false); // ローディング中かどうかを保持するための状態変数
 
   // コンポーネントのマウント時にキャラクター情報を取得
     useEffect(() => {
@@ -18,9 +18,18 @@ function App() {
   // API からキャラクター情報を取得する非同期関数
   const fetchCharacters = async (page: number) => {
     const apiUrl = 'https://narutodb.xyz/api/character';
+    setIsLoading(true);
     const response = await axios.get(apiUrl, {params: {page}});
     setCharacters(response.data.characters);
+    setIsLoading(false);
   };
+
+  // 前のページのキャラクター情報を取得する関数
+  const handlePrev = async () => {
+    const prevPage = page - 1;
+    await fetchCharacters(prevPage);
+    setPage(prevPage);
+  }
 
   // 次のページのキャラクター情報を取得する関数
   const handleNext = async () => {
@@ -32,48 +41,54 @@ function App() {
   // UI のレンダリング
   return (
     <div className="container">
-      <main>
-        <div className="cards-container">
-          {/* 取得したキャラクター情報をマッピングしてカードを表示 */}
-          {characters.map((character) => {
-            return (
-              <div className="card" key={character.id}>
-                <img 
-                  src={
-                    // キャラクターの画像があればそれを、なければダミー画像を表示
-                    character.images[0] != null
-                      ? character.images[0]
-                      : 'dummy.png'
-                  }
-                  alt="character" 
-                  className="card-image"
-                />
-                <div className="div card-content">
-                  <h3 className="card-title">{character.name}</h3>
-                  <p className="card-description">
-                    {/* キャラクターの説明があればそれを、なければ「説明なし」と表示 */}
-                    {character.debut?.appearsIn ?? 'Not'}
-                  </p>
-                  <div className="card-footer">
-                    <span className="affiliation">
-                      {/* キャラクターの所属があればそれを、なければ「所属なし」と表示 */}
-                      {character.personal?.affiliation ?? 'Not affiliation'}
-                    </span>
+      {isLoading ? (
+        <div>Now Loading...</div>
+      ) : (
+        <main>
+          <div className="cards-container">
+            {/* 取得したキャラクター情報をマッピングしてカードを表示 */}
+            {characters.map((character) => {
+              return (
+                <div className="card" key={character.id}>
+                  <img 
+                    src={
+                      // キャラクターの画像があればそれを、なければダミー画像を表示
+                      character.images[0] != null
+                        ? character.images[0]
+                        : 'dummy.png'
+                    }
+                    alt="character" 
+                    className="card-image"
+                  />
+                  <div className="div card-content">
+                    <h3 className="card-title">{character.name}</h3>
+                    <p className="card-description">
+                      {/* キャラクターの説明があればそれを、なければ「説明なし」と表示 */}
+                      {character.debut?.appearsIn ?? 'Not'}
+                    </p>
+                    <div className="card-footer">
+                      <span className="affiliation">
+                        {/* キャラクターの所属があればそれを、なければ「所属なし」と表示 */}
+                        {character.personal?.affiliation ?? 'Not affiliation'}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-        {/* ページャーのUI */}
-        <div className="pager">
-          <button className="prev">Previous</button>
-          <span className="page-number">{page}</span>
-          <button className="next" onClick={handleNext}>
-            Next
-          </button>
-        </div>
-      </main>
+              );
+            })}
+          </div>
+          {/* ページャーのUI */}
+          <div className="pager">
+            <button className="prev" onClick={handlePrev}>
+              Previous
+            </button>
+            <span className="page-number">{page}</span>
+            <button className="next" onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </main>
+      )}
     </div>
   );
 }
